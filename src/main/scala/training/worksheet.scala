dd8b4093f355fd28f159540a3165ea24c40a47df
jhon castro <jcastro@ias.com.co>
2018-04-03 02:58:14 -0500

    agregando neophytes

--- src/main/scala/training/worksheet.scala
+++ src/main/scala/training/worksheet.scala
@@ -18,5 +18,34 @@ object worksheet extends App {
   val nums = List(5, 1, 4, 3, 2)
   print(nums.filter(_ > 2).sorted)
 
+  println(" ")
+  val xs = 58 #:: 43 #:: 93 #:: Stream.empty
+  println(" ")
+  xs match {
+    case first #:: second #:: _ => print(first - second)
+    case _ => print(-1)
+  }
+  println(" ")
+  print(xs)
+
+  println(" ")
+  xs match {
+   case #::(first, #::(second, _)) => print(first - second)
+   case _ => print(-1)
+  }
+
+  println(" ")
+  val xsa = 3 :: 6 :: 12 :: Nil
+   xsa match {
+   case List(a, b) => print(a * b)
+   case List(a, b, c) => print(a + b + c)
+    case _ => print(0)
+   }
+  println(" ")
+  val xs3 = 3 :: 6 :: 12 :: 24 :: Nil
+  xs3 match {
+  case List(a, b, _*) => print(a * b)
+  case _ => print(0)
+  }
 
 }
